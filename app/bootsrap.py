3598081d7c3634525ff033e53d555f546fc8c0af
crowe-a <darkrenevatio@hotmail.com>
2022-08-28 22:17:49 +0300

    initialize

--- app/bootsrap.py
+++ app/bootsrap.py
@@ -3,9 +3,18 @@ from flask import Flask,render_template
 app=Flask(__name__)
 
 
+
+@app.route("/makale1")
+def makale1():
+    return render_template("includes/makaleler/makale1.html")
+
+@app.route("/makale2")
+def makale2():
+    return render_template("includes/makaleler/makale2.html")
+
 @app.route("/")
 def giriş():
-    return render_template("giriş.html")
+    return render_template("anasayfa.html")
 @app.route("/anasayfa")
 def anasayfa():
     return render_template ("anasayfa.html")
@@ -14,7 +23,31 @@ def anasayfa():
 def hakkımda():
     return render_template("hakkımda.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/makaleler")
+def makaleler():
+    return render_template("makaleler.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/fullpack")
+def full():
+    return render_template("fullpack.html")
 
 
 
-    
+
+
+
+
+
+
+
+
+
+
